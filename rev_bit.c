fe96535086a4a8fc59f51bdde9db49dd991b7d85
sathishintel <jsathishkumarc@gmail.com>
2017-01-24 11:15:04 +0530

    Create rev_bit.c

---
+++ rev_bit.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+unsigned int reverseBits(unsigned int num)
+{
+        unsigned int NO_OF_BITS = sizeof(num) * 8;
+        unsigned int reverse_num = 0, i, temp;
+
+        for (i = 0; i < NO_OF_BITS; i++)
+        {
+                temp = (num & (1 << i));
+                if(temp)
+                        reverse_num |= (1 << ((NO_OF_BITS - 1) - i));
+        }
+
+        return reverse_num;
+}
+
+/* Driver function to test above function */
+int main()
+{
+        unsigned int x = 1342177280; 
+        printf("%u", reverseBits(x));
+        getchar();
+}
