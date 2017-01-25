3cb4494973a56a6990e6e24f930b9412932332c8
sathishintel <jsathishkumarc@gmail.com>
2017-01-25 09:46:10 +0530

    Create little_to_bigendian.c

---
+++ little_to_bigendian.c
@@ -0,0 +1,4 @@
+swapped = ((num>>24)&0xff) | // move byte 3 to byte 0
+                    ((num<<8)&0xff0000) | // move byte 1 to byte 2
+                    ((num>>8)&0xff00) | // move byte 2 to byte 1
+                    ((num<<24)&0xff000000); // byte 0 to byte 3
